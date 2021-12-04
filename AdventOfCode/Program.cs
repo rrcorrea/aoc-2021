fd0a2a651fbec349d5c643a3c83954e9c1e02620
Romulo Correa <romulo.correa@poatek.com>
2021-12-04 12:27:28 -0300

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode;
+
+Console.WriteLine("Advent of Code");
+
+//Console.WriteLine("***Puzzle 1");
+//var puzzle1 = new Puzzle1();
+//puzzle1.Run1();
+//puzzle1.Run2();
+
+//Console.WriteLine("\n***Puzzle 2");
+//var puzzle2 =new Puzzle2();
+//puzzle2.Run1();
+//puzzle2.Run2();
+
+
+//Console.WriteLine("\n***Puzzle 3");
+//var puzzle3 = new Puzzle3();
+//puzzle3.Run1();
+//puzzle3.Run2();
+
+Console.WriteLine("\n***Puzzle 4");
+var puzzle4 = new Puzzle4();
+puzzle4.Run2();
